c6857e7b2551242a10de78f696af9491e2b21405
Andrew Edmonds <aedmonds@york.cuny.edu>
2025-09-25 12:16:57 -0500

    Update script that can compare trainings from the plots files

--- TrkQual/scripts/CompareTrainings.C
+++ TrkQual/scripts/CompareTrainings.C
@@ -3,12 +3,14 @@
 //
 void CompareTrainings(bool save_plots = false) {
 
+  std::string datadir = "/exp/mu2e/data/users/edmonds/TrkQualTraining/";
   std::string plotsdir = "/exp/mu2e/app/users/edmonds/plots/2025-01-29/";
   std::string plot_prefix = "Mu2eTrkQual_CompareTrainings_v1.1_v2.0";
 
   const int n_versions = 2;
   std::string versions[n_versions] = {"v1.1", "v2"};
   Int_t colors[n_versions] = {kRed, kBlue};
+  std::string datasets[n_versions] = {"", "_training"}; // for v1.1. and earlier, we did not store infidivual training, validation, mock results
 
   TCanvas* c_roc_curve = new TCanvas("c_roc_curve", "c_roc_curve");
   TCanvas* c_mom_res = new TCanvas("c_mom_res", "c_mom_res");
@@ -32,7 +34,8 @@ void CompareTrainings(bool save_plots = false) {
 
   for (int i_version = 0; i_version < n_versions; ++i_version) {
     std::string version = versions[i_version];
-    std::string filename = "TrkQual_ANN1_" + version + "_plots.root";
+    std::string dataset = datasets[i_version];
+    std::string filename = datadir + "/TrkQual_ANN1_" + version + "_plots.root";
     Int_t color = colors[i_version];
 
     TFile* file = new TFile(filename.c_str(), "READ");
@@ -71,7 +74,7 @@ void CompareTrainings(bool save_plots = false) {
     leg->AddEntry(gr_roc_curve, version.c_str(), "l");
 
     c_mom_res->cd();
-    std::string histname = "all_mom_res";
+    std::string histname = "all_mom_res" + dataset;
     TH1F* h_mom_res = (TH1F*) file->Get(histname.c_str());
     h_mom_res->SetTitle("High-Quality Mom Res");
     h_mom_res->SetLineWidth(2);
@@ -87,7 +90,7 @@ void CompareTrainings(bool save_plots = false) {
     for (int i_feature = 0; i_feature < n_features; ++i_feature) {
       c_high_qual_features[i_feature]->cd();
 
-      std::string histname = "high_qual_feature" + std::to_string(i_feature) + "_" + features[i_feature] + "_norm";
+      std::string histname = "high_qual_feature" + std::to_string(i_feature) + "_" + features[i_feature] + dataset + "_norm";
       TH1F* h_high_qual_feature = (TH1F*) file->Get(histname.c_str());
       h_high_qual_feature->SetTitle(std::string(features[i_feature] + " (High-Quality Tracks)").c_str());
       h_high_qual_feature->SetYTitle("normalized to unit area");
@@ -104,7 +107,7 @@ void CompareTrainings(bool save_plots = false) {
 
       c_low_qual_features[i_feature]->cd();
 
-      histname = "low_qual_feature" + std::to_string(i_feature) + "_" + features[i_feature] + "_norm";
+      histname = "low_qual_feature" + std::to_string(i_feature) + "_" + features[i_feature] + dataset +  "_norm";
       TH1F* h_low_qual_feature = (TH1F*) file->Get(histname.c_str());
       h_low_qual_feature->SetTitle(std::string(features[i_feature] + " (Low-Quality Tracks)").c_str());
       h_low_qual_feature->SetYTitle("normalized to unit area");
